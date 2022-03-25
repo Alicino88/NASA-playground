005f31cfe92453b3ec3f282448f276be543f80c6
Alice Moretti <ali.moret88@gmail.com>
2022-03-25 15:13:35 +0200

    Fetched pic, title and description, added useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  let [imgData, setImgData] = useState([]);
+
+  const getImage = () => {
+    /*NASA API https://api.nasa.gov/ */
+
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API}`
+    )
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log("there is an error");
+        }
+        console.log("resolved", response);
+        return response.json();
+      })
+      .then((data) => {
+        setImgData(data);
+        console.log(data.url);
+      })
+      .catch((err) => {
+        console.log("error retrieving data", err);
+      });
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={imgData.url} alt="space"></img>
+      <h1>{imgData.title}</h1>
+      <p>{imgData.explanation}</p>
     </div>
   );
 }
